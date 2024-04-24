8969df094bbcb07e11cf0f68d967b32d9d211df5
sai praveen <saipraveen@rimes.int>
2024-04-24 15:01:24 +0530

    head

--- application/views/Menu/Landing_page_top.php
+++ application/views/Menu/Landing_page_top.php
@@ -1,7 +1,7 @@
 <img src="<?php echo base_url('img/emblem.png'); ?>" alt="Emblem of India" height="50" style="padding-left: 1%;">
 
 <div style="flex: 1; text-align: center;">
-    <span class="text" style="font-family: 'Times New Roman';">WEATHER DECISION SUPPORT
+    <span class="text" style="font-family: 'Times New Roman'; font-size: 40px;">WEATHER DECISION SUPPORT
         SYSTEM</span>
 </div>
 
