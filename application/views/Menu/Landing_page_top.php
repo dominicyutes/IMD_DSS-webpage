3f21c89d2bb58633363910f5136a1eb889a21913
sai praveen <saipraveen@rimes.int>
2024-04-29 12:19:23 +0530

    top

--- application/views/Menu/Landing_page_top.php
+++ application/views/Menu/Landing_page_top.php
@@ -1,3 +1,21 @@
+<head>
+
+<style>
+     .text {
+            font-family: 'Archivo', sans-serif;
+            font-size: 2em;
+            font-weight: 600;
+            color: white;
+            letter-spacing: 3px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+</style>
+</head>
+
+<body>
+
 <img src="<?php echo base_url('img/emblem.png'); ?>" alt="Emblem of India" height="50" style="padding-left: 1%;">
 
 <div style="flex: 1; text-align: center;">
@@ -6,3 +24,4 @@
 </div>
 
 <img src="<?php echo base_url('img/imd_logo_a.png'); ?>" alt="IMD logo" height="50" style="padding-right: 1%;">
+</body>
